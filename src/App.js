9ea9ed1627bd11a741b8a6c6845c3b9fb7ee531b
abdullahzubair55 <abdullahdosta@gmail.com>
2020-06-15 16:40:22 +0500

    confused

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
 import React, { useState } from 'react';
 import './App.css';
 import Parent from './Parent';
-import ValueContext from './ValueContext';
+import ValueContext from './ValueContext'
 
 function App() {
-  let [number, setNumber] = useState (45);
-   
-  let value = 81;
+ // let [number, setNumber] = useState (45);
+ let value = useState (48);
+  
   return (
-
-   <ValueContext.Provider value= {value}>
-
-
-     <div>
-          Hello World;
-          <Parent> </Parent>
-          
-     </div>
-    
-
-   </ValueContext.Provider>
+    <div>
+      Hello World
+      <Parent></Parent>
+      <button onClick = {()=>setNumber(++number)}>Update Number</button>
+    </div>
   );
 }
 
---
+++ src/Child2.js
@@ -0,0 +1,17 @@
+import React, { useContext } from 'react';
+import './App.css';
+import ValueContext from './ValueContext';
+
+function Child2() {
+
+   
+  return (
+    <div>
+
+        Child 2
+        
+    </div>
+  );
+}
+
+export default Child2;
